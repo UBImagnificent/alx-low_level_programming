8f9caac5301ddddbd677148f9aa3c095e88b2177
UBImagnificient <brunouchendu@gmail.com>
2022-09-22 17:13:40 -0700

    Task 4-put.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * *main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming iz like building a multilingual puzzle");
+	return (0):
+}
