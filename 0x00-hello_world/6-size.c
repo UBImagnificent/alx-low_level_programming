52959f17a1c7314867938ffcd98ddff6c361e1ab
UBImagnificient <brunouchendu@gmail.com>
2022-09-22 17:57:16 -0700

    101-quote.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stduo.h>
+
+/**
+ * main - Prints the size of many var types
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: ⁰/cd byte(s)\n",sizeof(a));
+	printf("Size of an int: ⁰/cd bytes(s)\n",sizeof(b));
+	printf("Size of a long int: ⁰/cd bytes(s)\n",sizeof(c));
+	printf("Size of a long long int: ⁰/cd byte(s)\n",sizeof(d));
+	printf"(Size of a float: ⁰/cd byte(s)\n",sizeof(e));
+	return (0);
+
+}
