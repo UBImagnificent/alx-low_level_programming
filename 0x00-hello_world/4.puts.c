bbaae3339050670824be7471f56865d32e8fb245
UBImagnificent <brunouchendu@gmail.com>
2023-07-03 22:08:54 +0000

    Alx Tasks

---
+++ 0x00-hello_world/4.puts.c
@@ -0,0 +1,11 @@
+/*
+ * writing my very first C code
+ * code uses puts to print characters on standard output
+ */
+
+# include <stdio.h>
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
