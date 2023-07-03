469da3ace8d44031dd01aa96c8f631f6e6327c59
UBImagnificent <brunouchendu@gmail.com>
2023-07-03 22:43:35 +0000

    Alx Tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * using the printf function to print characters as output
+ * alx is amazing
+ */
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
