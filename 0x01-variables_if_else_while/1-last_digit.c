71844362c3cfa235e5ac7ceaddfc8b2a8039ae3d
UBImagnificent <brunouchendu@gmail.com>
2023-09-30 10:11:44 +0000

    Solving alx tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	int b = n % 10;
 
 	/**
 	 * the code above was provided by Alx
 	 * though I added the stdio.h header file
+	 * as well as the variable b
 	 * stdio.h allows ensures that printf functions as intended
 	 * beneath I will add the control statement to optimise the code
 	 * the program will now print the last digit of a radom number
 	 * as well as a little description of that digit
 	 * this is gotten through the if else statements
 	 */
-
-	int b = n % 10;
-	/* finds the last figit and stores it in a variable */
 
 	if (b > 5)
 	{
