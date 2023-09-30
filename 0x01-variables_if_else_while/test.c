807c4d7c04350c6102938200c4f31b206eb99de2
UBImagnificent <brunouchendu@gmail.com>
2023-09-30 11:03:06 +0000

    Solving alx tasks

---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+	 int alphabet;
+	 for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+	{
+		 putchar(alphabet);
+		 putchar('\n');
+
+	 }
+	 return (0);
+}
+
